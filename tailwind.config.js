76ad2b49aa0146262c20a54038a2d7cfbde72343
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2022-03-07 00:28:21 -0300

    build: install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [],
+  purge: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.html',
+    './*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
